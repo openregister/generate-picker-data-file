b48be47f312dbee118da8517070e8870ce5f90a6
Theodor Vararu <theo@vararu.org>
2017-06-16 15:09:46 +0100

    Use csv in fetchHandler

--- src/main/java/com/serverless/FetchHandler.java
+++ src/main/java/com/serverless/FetchHandler.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import uk.gov.Generator;
 import uk.gov.Fetcher;
@@ -28,15 +30,20 @@ public class FetchHandler implements RequestHandler<Map<String, Object>, ApiGate
 		String countryRegisterJson = "{}";
 		String territoryRegisterJson = "{}";
 		String ukRegisterJson = "{}";
+		String csv = "";
 		try {
 			countryRegisterJson = fetcher.get("https://country.register.gov.uk/records.json?page-size=500");
 			territoryRegisterJson = fetcher.get("https://territory.register.gov.uk/records.json?page-size=500");
 			ukRegisterJson = fetcher.get("https://uk.discovery.openregister.org/records.json?page-size=500");
+			csv = new String(Files.readAllBytes(Paths.get("location-picker-data.csv")));
 		} catch (IOException err) {
 			LOG.error("HTTP request IOException: " + err);
 		}
 
-		String responseBody = generator.runMultiple(countryRegisterJson, territoryRegisterJson, ukRegisterJson, "");
+		String responseBody = generator.runMultiple(
+			countryRegisterJson, territoryRegisterJson, ukRegisterJson,
+			csv
+		);
 
 		Map<String, String> headers = new HashMap<>();
 		headers.put("X-Powered-By", "AWS Lambda & Serverless");
