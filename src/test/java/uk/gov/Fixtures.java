2d8e4df737f99bca1963b1af1c53f2e950bd128f
Theodor Vararu <theo@vararu.org>
2017-06-22 15:07:33 +0100

    Update test Fixtures
    Add synonym CSV inputs and their resulting graph nodes.

--- src/test/java/uk/gov/Fixtures.java
+++ src/test/java/uk/gov/Fixtures.java
@@ -38,16 +38,55 @@ public class Fixtures {
 			+ "]"
 		+ "}";
 
-	public static final String csv = "";
+	public static final String territoryRegisterEntryPr = ""
+		+ "\"PR\": {"
+			+ "\"index-entry-number\": \"58\","
+			+ "\"entry-number\": \"58\","
+			+ "\"entry-timestamp\": \"2016-12-15T12:15:07Z\","
+			+ "\"key\": \"PR\","
+			+ "\"item\": ["
+				+ "{"
+					+ "\"official-name\": \"Commonwealth of Puerto Rico\","
+					+ "\"name\": \"Puerto Rico\","
+					+ "\"territory\": \"PR\""
+				+ "}"
+			+ "]"
+		+ "}";
 
-	public static final String gbGraphJson = ""
+	public static final String ukRegisterEntryWls = ""
+		+ "\"WLS\": {"
+			+ "\"index-entry-number\": \"4\","
+			+ "\"entry-number\": \"4\","
+			+ "\"entry-timestamp\": \"2016-08-04T08:05:35Z\","
+			+ "\"key\": \"WLS\","
+			+ "\"item\": ["
+				+ "{"
+					+ "\"official-name\": \"Wales\","
+					+ "\"uk\": \"WLS\","
+					+ "\"name\": \"Wales\""
+				+ "}"
+			+ "]"
+		+ "}";
+
+	public static final String csvHeader = ""
+		+ "Andy's key,Type,Name,Official-name,Code,Excluded,Territory belongs to,Territory belongs to code,Welsh,Passport applicant typos,Endonyms,Combined Synonyms,DWP Synonyms";
+
+	public static final String csvEntryGb = ""
+		+ "country:GB,country,United Kingdom,The United Kingdom of Great Britain and Northern Ireland,GB,,,,Y Deyrnas Unedig,\"Brtain\",\"UK\",";
+
+	public static final String csvEntryDe = ""
+		+ "country:DE,country,Germany,The Federal Republic of Germany,DE,,,,Yr Almaen,,Deutschland,\"Bundesrepublik\",";
+
+	public static final String graphJsonGb = ""
 		+ "\"country:GB\": {"
 			+ "\"names\": {"
-				+ "\"en-GB\": \"United Kingdom\""
+				+ "\"en-GB\": \"United Kingdom\","
+				+ "\"cy\": false"
 			+ "},"
 			+ "\"meta\": {"
 				+ "\"canonical\": true,"
 				+ "\"canonical-mask\": 1,"
+				+ "\"display-name\": true,"
 				+ "\"stable-name\": true"
 			+ "},"
 			+ "\"edges\": {"
@@ -55,24 +94,234 @@ public class Fixtures {
 			+ "}"
 		+ "}";
 
-	public static final String deGraphJson = ""
-		+ "\"country:DE\": {"
+	public static final String graphJsonGbWithCy = ""
+		+ "\"country:GB\": {"
 			+ "\"names\": {"
-				+ "\"en-GB\": \"Germany\""
+				+ "\"en-GB\": \"United Kingdom\","
+				+ "\"cy\": \"Y Deyrnas Unedig\""
 			+ "},"
 			+ "\"meta\": {"
 				+ "\"canonical\": true,"
 				+ "\"canonical-mask\": 1,"
+				+ "\"display-name\": true,"
 				+ "\"stable-name\": true"
 			+ "},"
 			+ "\"edges\": {"
 				+ "\"from\": []"
+			+ "}"
+		+ "}";
+
+	public static final String graphJsonNymUKGBNI = ""
+		+ "\"nym:The United Kingdom of Great Britain and Northern Ireland\": {"
+			+ "\"names\": {"
+				+ "\"en-GB\": \"The United Kingdom of Great Britain and Northern Ireland\","
+				+ "\"cy\": false"
+			+ "},"
+			+ "\"meta\": {"
+				+ "\"canonical\": false,"
+				+ "\"canonical-mask\": 0,"
+				+ "\"display-name\": true,"
+				+ "\"stable-name\": true"
+			+ "},"
+			+ "\"edges\": {"
+				+ "\"from\": [\"country:GB\"]"
+			+ "}"
+		+ "}";
+
+	public static final String graphJsonNymBrtain = ""
+		+ "\"nym:Brtain\": {"
+			+ "\"names\": {"
+				+ "\"en-GB\": \"Brtain\","
+				+ "\"cy\": false"
+			+ "},"
+			+ "\"meta\": {"
+				+ "\"canonical\": false,"
+				+ "\"canonical-mask\": 0,"
+				+ "\"display-name\": false,"
+				+ "\"stable-name\": false"
+			+ "},"
+			+ "\"edges\": {"
+				+ "\"from\": [\"country:GB\"]"
+			+ "}"
+		+ "}";
+
+	public static final String graphJsonNymGB = ""
+		+ "\"nym:GB\": {"
+			+ "\"names\": {"
+				+ "\"en-GB\": \"GB\","
+				+ "\"cy\": false"
+			+ "},"
+			+ "\"meta\": {"
+				+ "\"canonical\": false,"
+				+ "\"canonical-mask\": 0,"
+				+ "\"display-name\": false,"
+				+ "\"stable-name\": false"
+			+ "},"
+			+ "\"edges\": {"
+				+ "\"from\": [\"country:GB\"]"
+			+ "}"
+		+ "}";
+
+	public static final String graphJsonNymUK = ""
+		+ "\"nym:UK\": {"
+			+ "\"names\": {"
+				+ "\"en-GB\": \"UK\","
+				+ "\"cy\": false"
+			+ "},"
+			+ "\"meta\": {"
+				+ "\"canonical\": false,"
+				+ "\"canonical-mask\": 0,"
+				+ "\"display-name\": false,"
+				+ "\"stable-name\": false"
+			+ "},"
+			+ "\"edges\": {"
+				+ "\"from\": [\"country:GB\"]"
+			+ "}"
+		+ "}";
+
+	public static final String graphJsonDe = ""
+		+ "\"country:DE\": {"
+			+ "\"names\": {"
+				+ "\"en-GB\": \"Germany\","
+				+ "\"cy\": false"
+			+ "},"
+			+ "\"meta\": {"
+				+ "\"canonical\": true,"
+				+ "\"canonical-mask\": 1,"
+				+ "\"display-name\": true,"
+				+ "\"stable-name\": true"
+			+ "},"
+			+ "\"edges\": {"
+				+ "\"from\": []"
+			+ "}"
+		+ "}";
+
+	public static final String graphJsonDeWithCy = ""
+		+ "\"country:DE\": {"
+			+ "\"names\": {"
+				+ "\"en-GB\": \"Germany\","
+				+ "\"cy\": \"Yr Almaen\""
+			+ "},"
+			+ "\"meta\": {"
+				+ "\"canonical\": true,"
+				+ "\"canonical-mask\": 1,"
+				+ "\"display-name\": true,"
+				+ "\"stable-name\": true"
+			+ "},"
+			+ "\"edges\": {"
+				+ "\"from\": []"
+			+ "}"
+		+ "}";
+
+	public static final String graphJsonNymFRG = ""
+		+ "\"nym:The Federal Republic of Germany\": {"
+			+ "\"names\": {"
+				+ "\"en-GB\": \"The Federal Republic of Germany\","
+				+ "\"cy\": false"
+			+ "},"
+			+ "\"meta\": {"
+				+ "\"canonical\": false,"
+				+ "\"canonical-mask\": 0,"
+				+ "\"display-name\": true,"
+				+ "\"stable-name\": true"
+			+ "},"
+			+ "\"edges\": {"
+				+ "\"from\": [\"country:DE\"]"
+			+ "}"
+		+ "}";
+
+	public static final String graphJsonNymDeutschland = ""
+		+ "\"nym:Deutschland\": {"
+			+ "\"names\": {"
+				+ "\"en-GB\": \"Deutschland\","
+				+ "\"cy\": false"
+			+ "},"
+			+ "\"meta\": {"
+				+ "\"canonical\": false,"
+				+ "\"canonical-mask\": 0,"
+				+ "\"display-name\": false,"
+				+ "\"stable-name\": false"
+			+ "},"
+			+ "\"edges\": {"
+				+ "\"from\": [\"country:DE\"]"
+			+ "}"
+		+ "}";
+
+	public static final String graphJsonNymDE = ""
+		+ "\"nym:DE\": {"
+			+ "\"names\": {"
+				+ "\"en-GB\": \"DE\","
+				+ "\"cy\": false"
+			+ "},"
+			+ "\"meta\": {"
+				+ "\"canonical\": false,"
+				+ "\"canonical-mask\": 0,"
+				+ "\"display-name\": false,"
+				+ "\"stable-name\": false"
+			+ "},"
+			+ "\"edges\": {"
+				+ "\"from\": [\"country:DE\"]"
+			+ "}"
+		+ "}";
+
+	public static final String graphJsonNymBundesrepublik = ""
+		+ "\"nym:Bundesrepublik\": {"
+			+ "\"names\": {"
+				+ "\"en-GB\": \"Bundesrepublik\","
+				+ "\"cy\": false"
+			+ "},"
+			+ "\"meta\": {"
+				+ "\"canonical\": false,"
+				+ "\"canonical-mask\": 0,"
+				+ "\"display-name\": false,"
+				+ "\"stable-name\": false"
+			+ "},"
+			+ "\"edges\": {"
+				+ "\"from\": [\"country:DE\"]"
+			+ "}"
+		+ "}";
+
+	public static final String graphJsonPr = ""
+		+ "\"territory:PR\": {"
+			+ "\"names\": {"
+				+ "\"en-GB\": \"Puerto Rico\","
+				+ "\"cy\": false"
+			+ "},"
+			+ "\"meta\": {"
+				+ "\"canonical\": true,"
+				+ "\"canonical-mask\": 1,"
+				+ "\"display-name\": true,"
+				+ "\"stable-name\": true"
+			+ "},"
+			+ "\"edges\": {"
+				+ "\"from\": []"
+			+ "}"
+		+ "}";
+
+	public static final String graphJsonWls = ""
+		+ "\"uk:WLS\": {"
+			+ "\"names\": {"
+				+ "\"en-GB\": \"Wales\","
+				+ "\"cy\": false"
+			+ "},"
+			+ "\"meta\": {"
+				+ "\"canonical\": false,"
+				+ "\"canonical-mask\": 0,"
+				+ "\"display-name\": true,"
+				+ "\"stable-name\": true"
+			+ "},"
+			+ "\"edges\": {"
+				+ "\"from\": [\"country:GB\"]"
 			+ "}"
 		+ "}";
 
 	// Turns a list of JSON entries into a big JSON object with the strings as keys.
-	public static String joinEntries(String[] registerEntries) {
+	public static String joinJsonEntries(String[] registerEntries) {
 		return "{" + String.join(",", registerEntries) + "}";
+	}
+
+	public static String joinCsvEntries(String [] csvEntries) {
+		return String.join("\n", csvEntries);
 	}
 
 	// Pipe through Jackson so we get a JSON string with consistent whitespacing and ordering.
@@ -80,23 +329,58 @@ public class Fixtures {
 		return objectMapper.readTree(json).toString();
 	}
 
-	public static String countryRegisterOnlyGb () {
+	public static String countryRegisterOnlyGb() {
 		String[] registerEntries = {countryRegisterEntryGb};
-		return joinEntries(registerEntries);
+		return joinJsonEntries(registerEntries);
 	}
 
-	public static String countryRegisterOnlyGbDe () {
+	public static String countryRegisterOnlyGbDe() {
 		String[] registerEntries = {countryRegisterEntryGb, countryRegisterEntryDe};
-		return joinEntries(registerEntries);
+		return joinJsonEntries(registerEntries);
 	}
 
-	public static String graphOnlyGb () throws Exception {
-		String[] graphEntries = {gbGraphJson};
-		return prettyJson(joinEntries(graphEntries));
+	public static String territoryRegisterOnlyPr() {
+		String[] registerEntries = {territoryRegisterEntryPr};
+		return joinJsonEntries(registerEntries);
 	}
 
-	public static String graphOnlyGbDe () throws Exception {
-		String[] graphEntries = {gbGraphJson, deGraphJson};
-		return prettyJson(joinEntries(graphEntries));
+	public static String ukRegisterOnlyWls() {
+		String[] registerEntries = {ukRegisterEntryWls};
+		return joinJsonEntries(registerEntries);
+	}
+
+	public static String csvOnlyGb() {
+		String [] csvEntries = {csvHeader, csvEntryGb};
+		return joinCsvEntries(csvEntries);
+	}
+
+	public static String csvOnlyGbDe() {
+		String [] csvEntries = {csvHeader, csvEntryGb, csvEntryDe};
+		return joinCsvEntries(csvEntries);
+	}
+
+	public static String graphOnlyGb() throws Exception {
+		String[] graphEntries = {graphJsonGb};
+		return prettyJson(joinJsonEntries(graphEntries));
+	}
+
+	public static String graphOnlyGbDe() throws Exception {
+		String[] graphEntries = {graphJsonGb, graphJsonDe};
+		return prettyJson(joinJsonEntries(graphEntries));
+	}
+
+	public static String graphWithSynonymsOnlyGb() throws Exception {
+		String[] graphEntries = {graphJsonGbWithCy, graphJsonNymUKGBNI, graphJsonNymBrtain, graphJsonNymGB, graphJsonNymUK};
+		return prettyJson(joinJsonEntries(graphEntries));
+	}
+
+	public static String graphWithSynonymsOnlyGbDe() throws Exception {
+		String[] graphEntries = {graphJsonGbWithCy, graphJsonNymUKGBNI, graphJsonNymBrtain, graphJsonNymGB, graphJsonNymUK, graphJsonDeWithCy, graphJsonNymFRG, graphJsonNymDeutschland, graphJsonNymDE, graphJsonNymBundesrepublik};
+		return prettyJson(joinJsonEntries(graphEntries));
+	}
+
+	public static String graphThreeRegistersWithCsv() throws Exception {
+		String[] graphEntries = {graphJsonGbWithCy, graphJsonNymUKGBNI, graphJsonNymBrtain, graphJsonNymGB, graphJsonNymUK, graphJsonDeWithCy, graphJsonNymFRG, graphJsonNymDeutschland, graphJsonNymDE, graphJsonNymBundesrepublik, graphJsonPr, graphJsonWls};
+		return prettyJson(joinJsonEntries(graphEntries));
 	}
 }
