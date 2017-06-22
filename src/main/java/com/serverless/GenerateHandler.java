4bd34bb01bb8735c406f575d11ab89e5fbfd13a1
Theodor Vararu <theo@vararu.org>
2017-06-22 15:50:05 +0100

    Create /fetch serverless handler
    `/generate` is an endpoint that you can post a JSON of the country
    register and it will return a data file based on just that.
    
    `/fetch` is an endpoint that will go off and fetch the country,
    territory, uk registers and the synonym CSV and return a full data
    file.

--- src/main/java/com/serverless/GenerateHandler.java
+++ src/main/java/com/serverless/GenerateHandler.java
@@ -1,5 +1,6 @@
 package com.serverless;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,23 +12,27 @@ import org.apache.log4j.Logger;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+public class GenerateHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
-	private static final Logger LOG = Logger.getLogger(Handler.class);
+	private static final Logger LOG = Logger.getLogger(GenerateHandler.class);
 
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
 		LOG.info("received: " + input);
+
 		String postJson = input.get("body").toString();
-		String responseBody = "";
+
+		String responseBody = "{}";
 		try {
 			responseBody = Generator.run(postJson, "");
-		} catch (Exception err) {
-			LOG.error("Generator.run error: " + err);
+		} catch (IOException err) {
+			LOG.error("Generator.run IOException: " + err);
 		}
+
 		Map<String, String> headers = new HashMap<>();
 		headers.put("X-Powered-By", "AWS Lambda & Serverless");
 		headers.put("Content-Type", "application/json");
+
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
 				.setRawBody(responseBody)
