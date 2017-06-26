50e462c98ecd037f006b5b4c0cc097e59f79702c
Theodor Vararu <theo@vararu.org>
2017-06-26 19:20:15 +0100

    Update deploy.sh
    Take into account current branch for use on Travis.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
-./gradlew build && sls deploy
+BRANCH=${TRAVIS_BRANCH:-$(git rev-parse --abbrev-ref HEAD)} 
+
+if [[ $BRANCH == 'master' ]]; then
+  STAGE="prod"
+elif [[ $BRANCH == 'develop' ]]; then
+  STAGE="dev"
+fi
+
+if [ -z ${STAGE+x} ]; then
+  echo "Not deploying changes";
+  exit 0;
+fi
+
+echo "Deploying from branch $BRANCH to stage $STAGE"
+
+./gradlew build &&
+sls deploy --stage $STAGE --region $AWS_REGION
